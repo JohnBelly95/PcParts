e6bdb0bc4c53eb5319e9d3e26f878759afaf86f1
JohnBelly95 <johnbelly95@gmail.com>
2014-04-24 14:38:28 +0300

    Update PcParts.java

--- PcParts.java
+++ PcParts.java
@@ -3,14 +3,14 @@ public class PcParts{
 	private String Manufacturer;
 	private int modelYear;
 	private int Price;
-	
-	public Model(){
-		modelName = Shizzle;
-		Manufacturer= Drizzle;
+
+	public PcParts(){
+		modelName = "Shizzle";
+		Manufacturer= "Drizzle";
 		modelYear = 1995;
 		Price = 1000000;
 	}
-	public Model( String modelName, String Manufacturer, int modelYear, int Price){
+	public PcParts(String modelName, String Manufacturer, int modelYear, int Price){
 		this.modelName = modelName;
 		this.Manufacturer = Manufacturer;
 		this.modelYear = modelYear;
