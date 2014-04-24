b2a2d8eecf129547947b876c98e9ca8ec781f4da
JohnBelly95 <johnbelly95@gmail.com>
2014-04-24 19:28:54 +0300

    Update RAM.java

--- RAM.java
+++ RAM.java
@@ -15,6 +15,24 @@ public class RAM extends Hardware{
 		this.Size = Size;
 		this.Speed=Speed;
 	}
+	public String getType(){
+		return Type;
+	}
+	public void setType(String Type){
+		this.Type = Type;
+	}
+	public int getSize(){
+		return Size;
+	}
+	public void setSize(int Size){
+		this.Size = Size;
+	}
+	public int getSpeed(){
+		return Speed;
+	}
+	public void setSpeed(int Speed){
+		this.Speed = Speed;
+	}
 	public String toString(){
 		return("This is a bundle of " + getmodelName() + " " + Size + " GB " + Type + " " + Speed +" Ghz RAM. It's from " + getManufacturer() + " manufactured in "+ getmodelYear() + " and it costs " + getPrice() + " Euros.");
 	}
