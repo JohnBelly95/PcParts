eb30268d53312a3f766da4497498e8bd4e4b63f6
JohnBelly95 <johnbelly95@gmail.com>
2014-04-24 16:38:53 +0300

    Update Motherboard.java

--- Motherboard.java
+++ Motherboard.java
@@ -16,5 +16,5 @@ public class Motherboard extends Hardware{
 	}
 	public String toString(){
 		return("This is a Motherboard from" + getManufacturer() + " from " + getmodelYear() + ". It supports up to " + RAMSize + " GB of RAM and has " + ExpansionSlots + "Expansion Slots. It's a"+ getmodelName() + " model for the " + Chip + " Chipset. Its price is " + getPrice() + " Euros.");
-	}
+	}//This is here for testing purposes feel free to ignore.
 }
