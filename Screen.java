98aaff3fd6704d202782c806d5c917cba6c60174
dorarhcp <doradervi@live.com>
2014-04-24 17:49:54 +0300

    Create Screen.java

---
+++ Screen.java
@@ -0,0 +1,32 @@
+public class Screen extends Peripherals{
+
+	private String Type;
+	private double Size;
+	private int Resolution;
+	private int HDMIport;
+	private int DVIport;
+	private int COMPOSITEport;
+	
+	public Screen(){
+		super();
+		Type = " ";
+		Size = 0;
+		Resolution = 0;
+		HDMIport = 0;
+		DVIport = 0;
+		COMPOSITEport = 0;
+	}
+	public Screen(String Type, double Size, int Resolution, int HDMIport, int DVIport, int COMPOSITEport, String modelName, String Manufacturer, int modelYear, int Price){
+		super(modelName, Manufacturer, modelYear, Price);
+		this.Type = Type;
+		this.Size = Size;
+		this.Resolution = Resolution;
+		this.HDMIport = HDMIport;
+		this.DVIport = DVIport;
+		this.COMPOSITEport = COMPOSITEport;
+	}
+	public String toString(){
+		return("This is an " + getManufacturer() + " " + getmodelName() + " Screen from " + getmodelYear() +", with " + Type + " type ," + Size + " inches dimensions ," +Resolution+ " pixels resolution ," +HDMIport+ "HDMI ports ," +DVIport+ " DVI ports and" +COMPOSITEport+ " Compose ports"  +". It's price is " + getPrice() + " Euros.");
+	}
+	
+}
