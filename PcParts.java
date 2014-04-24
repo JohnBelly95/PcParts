5ae1adba2fd48f3ac49d547a0424aadbf20eafe2
JohnBelly95 <johnbelly95@gmail.com>
2014-04-24 14:31:24 +0300

    Create PcParts.java

---
+++ PcParts.java
@@ -0,0 +1,16 @@
+public class PcParts{
+	private String modelName;
+	private String Manufacturer;
+	private int modelYear;
+	private int Price;
+	
+	public Model(){
+		modelName = Shizzle;
+		Manufacturer= Drizzle;
+		modelYear = 1995;
+		Price = 1000000;
+	}
+	public Model( String modelName, String Manufacturer, int modelYear, int Price){
+		this.modelName
+	
+}
