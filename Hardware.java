db0b68958654c089f704c4ad735016492ce55517
JohnBelly95 <johnbelly95@gmail.com>
2014-04-24 15:57:19 +0300

    Create Hardware.java

---
+++ Hardware.java
@@ -0,0 +1,8 @@
+public class Hardware extends PcParts{
+	public Hardware(){
+		super();
+	}
+	public Hardware(String modelName, String Manufacturer, int modelYear, int Price){
+		super(modelName, Manufacturer, modelYear, Price);
+	}
+}
