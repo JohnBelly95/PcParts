50a24e736549a05c59ad143be57eb65aca602ceb
dorarhcp <doradervi@live.com>
2014-04-25 15:33:19 +0300

    Update Keyboard.java

--- Keyboard.java
+++ Keyboard.java
@@ -5,12 +5,23 @@ public class Keyboard extends Peripherals{
 		super();
 		Connection = "Wired" ;
 	}
+	
 	public Keyboard(String Connection, String modelName, String Manufacturer, int modelYear, int Price){
 		super(modelName, Manufacturer, modelYear, Price);
 		this.Connection = Connection;
 	}
+	
+	public String getConnection(){
+		return Connection;
+	}
+	
+	public void setConnection(String Connection){
+		this.Connection = Connection;
+	}
+	
 	public String toString(){
 		return("This is an " + getManufacturer() + " " + getmodelName() + " Keyboard from " + getmodelYear() +", with " + Connection + " connection "  + ". It's price is " + getPrice() + " Euros.");
 	}
 	
 }
+
