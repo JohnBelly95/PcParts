8e760877c25361bd368c0a91715ba00dd27c2fc9
dorarhcp <doradervi@live.com>
2014-04-25 15:34:07 +0300

    Update Mouse.java

--- Mouse.java
+++ Mouse.java
@@ -7,13 +7,31 @@ public class Mouse extends Peripherals{
 		Connection = "Wired" ;
 		Technology = "Laser" ;
 	}
+	
 	public Mouse(String Connection, String Technology, String modelName, String Manufacturer, int modelYear, int Price){
 		super(modelName, Manufacturer, modelYear, Price);
 		this.Connection = Connection;
 		this.Technology = Technology ;
 	}
+	
+	public String getConnection(){
+		return Connection;
+	}
+	
+	public void setConnection(String Connection){
+		this.Connection = Connection;
+	}
+	public String getTechnology(){
+		return Technology;
+	}
+	
+	public void setTechnology(String Technology){
+		this.Technology = Technology;
+	}
+	
 	public String toString(){
 		return("This is an " + getManufacturer() + " " + getmodelName() + " Mouse from " + getmodelYear() +", with " + Connection + " connection and " + Technology + " technology " + ". It's price is " + getPrice() + " Euros.");
 	}
 	
 }
+
