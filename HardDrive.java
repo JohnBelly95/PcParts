e1bff988c500b029d88321451cdf683cd4f275be
JohnBelly95 <johnbelly95@gmail.com>
2014-04-25 14:41:34 +0300

    Update HardDrive.java

--- HardDrive.java
+++ HardDrive.java
@@ -11,7 +11,20 @@ public class HardDrive extends Hardware{
 	}
 	public HardDrive(String Type, int Size, int Width,  String modelName, String Manufacturer, int modelYear, int Price){
 		super(modelName, Manufacturer, modelYear, Price);
-		this.Type = Type;
+		switch(Type){
+			case"DDR":
+				this.Type = "DDR";
+				break;
+			case"DDR2": 
+				this.Type = "DDR2";
+				break;
+			case"DDR3": 
+				this.Type = "DDR3";
+				break;
+			default: 
+				this.Type = "Invalid Memory Type";
+				break;
+		}
 		this.Size = Size;
 		this.Width=Width;
 	}
@@ -34,6 +47,7 @@ public class HardDrive extends Hardware{
 		this.Width = Width;
 	}
 	public String toString(){
+		System.out.println("===============================================================");
 		return("This is a " + getmodelName()+" "+ Width + " inch " + Type +" with " + Size + " GB of space. It was manufactured in " + getmodelYear() +" from " + getManufacturer()+ " and it costs " + getPrice() + " Euros");
 	}
 }
