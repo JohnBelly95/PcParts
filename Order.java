27291380aa9c6eb1a18ef24f55ccd2ae42a3de4e
JohnBelly95 <johnbelly95@gmail.com>
2014-05-01 20:52:37 +0300

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,19 +1,24 @@
-import java.io.*;
-import java.util.Scanner;
-
 public class Order{
-	private int j = 1;
-	private String str;
-	Scanner input = new Scanner(System.in);
-
-	public Order(String Name){
-		System.out.println("What is the customer ordering ?");
-		str = input.nextLine();
-		while(j!=2)
-			switch(str){
-				case"Motherboard":
-					System.out.println("Please input the specifications for the desired product")
-			}
-		}
+	private String name, orderDate, arrivalDate, state;
+	private int phone, orderNo, Price;
+	private PcParts thing;
+	
+	public Order(){
+		name = "JZ";
+		phone = 2109330666;
+		orderDate="12/12/12";
+		arrivalDate="1/1/13";
+	}
+	public static void main(String[] args) {											//for testing purposes.
+		PcParts i54670k = new CPU(3.4,4,"i5-4670k","Intel",2013,200);
+		Order cpu = new Order("GP",123,"12/12/12","12/12/12",i54670k);
+		System.out.println(cpu.thing.getManufacturer());
+	}
+	public Order(String name, int phone, String orderDate, String arrivalDate, PcParts thing){
+		this.name = name;
+		this.phone = phone;
+		this.orderDate = orderDate;
+		this.arrivalDate = arrivalDate;
+		this.thing = thing;
 	}
 }
