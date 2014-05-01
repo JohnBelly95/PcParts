b2f85ecc44024c7b046a10155e13152af0f62d77
JohnBelly95 <johnbelly95@gmail.com>
2014-05-01 22:07:26 +0300

    Update Test404.java

--- Test404.java
+++ Test404.java
@@ -1,12 +1,12 @@
 public class Test404{
 	public static void main(String[] args) {
-		CPU i54670k = new CPU(3.4,4,"i5-4670k","Intel",2013,200); 
+		PcParts i54670k = new CPU(3.4,4,"i5-4670k","Intel",2013,200); 
 		System.out.println(i54670k);
-		Motherboard Z87k = new Motherboard("Intel",32,7,"Z87-K","Asus",2013,112);
+		PcParts Z87k = new Motherboard("Intel",32,7,"Z87-K","Asus",2013,112);
 		System.out.println(Z87k);
-		CPU topkek = new CPU();
+		PcParts topkek = new CPU();
 		System.out.println(topkek);
-		Keyboard BlackWidow = new Keyboard("Wired","Black Widow","Razer",2014,100);
+		PcParts BlackWidow = new Keyboard("Wired","Black Widow","Razer",2014,100);
 		System.out.println(BlackWidow);
 		BlackWidow.setPrice(220);
 		System.out.println(BlackWidow);
