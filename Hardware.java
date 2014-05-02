058d7245d428c43c1b85a9800ad18125129785f9
JohnBelly95 <johnbelly95@gmail.com>
2014-05-02 20:55:03 +0300

    Update Hardware.java

--- Hardware.java
+++ Hardware.java
@@ -2,8 +2,10 @@ public class Hardware extends PcParts{
 	private int sale;
 	public Hardware(){
 		super();
+		sale = 0;
 	}
 	public Hardware(String modelName, String Manufacturer, int modelYear, int Price){
 		super(modelName, Manufacturer, modelYear, Price);
+		sale = 0;
 	}
 }
