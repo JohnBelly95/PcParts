9c5d53ccc8f1fe398c0ce5deeca5f8fd4603ec52
JohnBelly95 <johnbelly95@gmail.com>
2014-05-02 20:50:51 +0300

    Update GPU.java

--- GPU.java
+++ GPU.java
@@ -1,35 +1,33 @@
 public class GPU extends Hardware{
 	private String Chipset;
 	private int Memory, sale;
-	
+
 	public GPU(){
 		super();
 		Chipset = " ";
 		Memory = 0;
-		sale = super.sale;
 	}
-	
+
 	public GPU(String Chipset, int Memory, String modelName, String Manufacturer, int modelYear, int Price){
 		super(modelName, Manufacturer, modelYear, Price);
 		this.Chipset = Chipset;
 		this.Memory = Memory;
-		sale = super.sale;
 	}
-	
+
 	public String getChipset(){
 		return Chipset;
 	}
 	public void setChipset(String Chipset){
 		this.Chipset = Chipset;
 	}
-	
+
 	public int getMemory(){
 		return Memory;
 	}
 	public void setMemory(int Memory){
 		this.Memory = Memory;
 	}
-	
+
 	public String toString(){
 		return ("This is a GPU from " + getManufacturer() + " from " + getmodelYear() + ". It has " + Memory + " GB of memory and it's a "+ getmodelName() + " model for the " + Chipset + " Chipset. Its price is " + getPrice() + " Euros.");
 	}
