d2ad4275c9f8a2d674dbf4056ed241bab0eeccdb
JohnBelly95 <johnbelly95@gmail.com>
2014-05-02 20:49:39 +0300

    Update CPU.java

--- CPU.java
+++ CPU.java
@@ -7,13 +7,11 @@ public class CPU extends Hardware{
 		super();
 		Speed = 2.9;
 		CoreCount = 2;
-		sale = super.sale;
 	}
 	public CPU(double Speed, int CoreCount, String modelName, String Manufacturer, int modelYear, int Price){
 		super(modelName, Manufacturer, modelYear, Price);
 		this.Speed = Speed;
 		this.CoreCount = CoreCount;
-		sale = super.sale;
 	}
 	public double getSpeed(){
 		return Speed;
