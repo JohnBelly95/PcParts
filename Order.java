df5ec8aa936baa7d2693227e5a93ae9c0355756b
JohnBelly95 <johnbelly95@gmail.com>
2014-05-02 21:01:28 +0300

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,24 +1,40 @@
+import java.util.*;
+import java.text.*;
+
 public class Order{
-	private String name, orderDate, arrivalDate, state;
-	private int phone, orderNo, Price;
+	private String name, expectedDate, state, str;
+	private int phone, orderNo, price, sale;
 	private PcParts thing;
+	private Scanner input = new Scanner(System.in);
+	SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
+	
 	
 	public Order(){
-		name = "JZ";
-		phone = 2109330666;
-		orderDate="12/12/12";
-		arrivalDate="1/1/13";
+		
 	}
-	public static void main(String[] args) {											//for testing purposes.
+	/*public static void main(String[] args) {											//for testing purposes.
 		PcParts i54670k = new CPU(3.4,4,"i5-4670k","Intel",2013,200);
 		Order cpu = new Order("GP",123,"12/12/12","12/12/12",i54670k);
 		System.out.println(cpu.thing.getManufacturer());
-	}
-	public Order(String name, int phone, String orderDate, String arrivalDate, PcParts thing){
-		this.name = name;
-		this.phone = phone;
-		this.orderDate = orderDate;
-		this.arrivalDate = arrivalDate;
+	}*/
+	public Order(PcParts thing){
+		System.out.println("You will now be asked to input the customers credentials");
+		System.out.println("Please enter the customers full name.");
+		System.out.print(">");
+		name = input.nextLine();
+		System.out.println("Please enter the customers phone.");
+		System.out.print(">");
+		phone = input.nextInt();
+		System.out.println("When do we expect the item to be available ?");
+		System.out.print(">");
+		expectedDate = input.nextLine();
+		Date date = new Date();
 		this.thing = thing;
+		System.out.println("Is the desired item on sale ? (Y/N)");
+		System.out.print(">");
+		str = input.nextLine();
+		if(str.equals(Y)){
+			thing.applySale();
+		}
 	}
 }
