bbb7f699d0dd14bf3f6f32df157fb9c021f5a592
JohnBelly95 <johnbelly95@gmail.com>
2014-05-02 20:51:46 +0300

    Update Peripherals.java

--- Peripherals.java
+++ Peripherals.java
@@ -2,9 +2,11 @@ public class Peripherals extends PcParts{
 	private int sale;
 	public Peripherals (){
 		super();
+		sale;
 	}
 	public Peripherals(String modelName, String Manufacturer, int modelYear, int Price){
 		super(modelName, Manufacturer, modelYear, Price);
+		sale;
 	}
 	
 }
