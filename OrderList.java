f43d58e70bc03b8e60ee08d7390c1e981d0d54ad
dorarhcp <doradervi@live.com>
2014-05-05 21:44:53 +0300

    Update OrderList.java

--- OrderList.java
+++ OrderList.java
@@ -1,6 +1,12 @@
+
 import java.util.*;
 
 public class OrderList{
 	private List<Order> orderList = new ArrayList<Order>();
 	
+	public void showOrders(){
+		for (int i=0; i < orderList.size(); i++){
+			System.out.println(orderList.get(i));
+		}
+	}	
 }
