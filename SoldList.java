b138f00916430ce05647fbacef1a9dcaf611650d
dorarhcp <doradervi@live.com>
2014-05-05 21:45:19 +0300

    Update SoldList.java

--- SoldList.java
+++ SoldList.java
@@ -1,5 +1,11 @@
 import java.util.*;
 
 public class SoldList{
-	private List<Sell> soldList = new ArrayList<Sell>();
+	private List<Sell> sales = new ArrayList<Sell>();
+	
+	public void showSales(){
+		for (int i=0; i < soldList.size(); i++){
+			System.out.println(soldList.get(i));
+		}
+	}
 }
