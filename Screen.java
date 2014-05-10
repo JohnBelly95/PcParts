12b301ec58d866e9d19c22998fd05a609b3551ad
dorarhcp <doradervi@live.com>
2014-05-10 19:26:27 +0300

    Update Screen.java

--- Screen.java
+++ Screen.java
@@ -1,11 +1,8 @@
 public class Screen extends Peripherals{
 
-	private String Type;
+	private String Type, Resolution;
 	private double Size;
-	private String Resolution;
-	private int HDMIport;
-	private int DVIport;
-	private int COMPOSITEport;
+	private int HDMIport, DVIport, COMPOSITEport;
 	private static boolean isHardware;
 	
 	public Screen(){
@@ -28,6 +25,50 @@ public class Screen extends Peripherals{
 		this.COMPOSITEport = COMPOSITEport;
 		isHardware = super.isHardware;
 	}
+	
+	public String getType(){
+		return Type;
+	}
+	public void setType(String Type){
+		this.Type = Type;
+	}
+	
+	public double getSize(){
+		return Size;
+	}
+	public void setSize(double Size){
+		this.Size = Size;
+	}
+	
+	public String getResolution(){
+		return Resolution;
+	}
+	public void setResolution(String Resolution){
+		this.Resolution = Resolution;
+	}
+	
+	public int getHDMIport(){
+		return HDMIport;
+	}
+	public void setHDMIport(int HDMIport){
+		this.HDMIport = HDMIport;
+	}
+	
+	public int getDVIport(){
+		return DVIport;
+	}
+	public void setDVIport(int DVIport){
+		this.DVIport = DVIport;
+	}
+	
+	public int getCOMPOSITEport(){
+		return COMPOSITEport;
+	}
+	public void setCOMPOSITEport(int COMPOSITEport){
+		this.COMPOSITEport = COMPOSITEport;
+	}
+	
+	
 	public String toString(){
 		System.out.println("===============================================================");
 		return("This is an " + getManufacturer() + " " + getmodelName() + " Screen from " + getmodelYear() +" , with " + Type + " type , " + Size + " inches , " +Resolution+ " pixels resolution , " +HDMIport+ " HDMI ports , " +DVIport+ " DVI ports and " +COMPOSITEport+ " Compose ports "  +". It's price is " + getPrice() + " Euros.");
