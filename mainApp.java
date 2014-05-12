9cc4bfc496db340671532bd4500cf502cd74e3c7
dorarhcp <doradervi@live.com>
2014-05-12 10:51:02 +0300

    JB kati trexei me to break

--- mainApp.java
+++ mainApp.java
@@ -195,13 +195,10 @@ public class mainApp{
 		reply = input.nextInt();
 		if ( reply==0){
 			break;
-		}else{			
-			if (reply-- > ordersList.size()){
-				System.out.println("You picked "+reply+" . There are not so many orders to list.");
-				System.out.println("Please try again !");
-				break;
-			}
-			break;		
+		}else if (reply-- > ordersList.size()){
+			System.out.println("You picked "+reply+" . There are not so many orders to list.");
+			System.out.println("Please try again !");
+			//break;
 		}else{
 			System.out.println(ordersList.get(reply--));
 			System.out.println("Would you like to change the order's status ? (Y/N) ");
@@ -212,9 +209,11 @@ public class mainApp{
 				//status[reply--] = true;
 				System.out.println("The order's status is now 'Available' .");
 				Sell sl = new Sell(ordersList.get(reply--).getthing, ordersList.get(reply--).getname, ordersList.get(reply--).getphone, ordersList.get(reply--).getfp);//prepei na paroume ta stoixeia apo tin order
-				soldList.add(sl);				
-			}			
-		}		
+				soldList.add(sl);	
+			}
+			break;
+		}
+		//break;
 	}
 	public static void showSales(){
 		for (int i=0; i < soldList.size(); i++){
