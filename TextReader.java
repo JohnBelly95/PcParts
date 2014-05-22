377c0f39ea235b5272c341274d4f6f683b0109c6
dorarhcp <doradervi@live.com>
2014-05-22 14:45:03 +0300

    Create TextReader.java

---
+++ TextReader.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class TextReader{
+
+	private List<Stock> stockList;
+	private List<Order> orderList;
+	private List<Sell> soldList;
+	static BufferedReader input;
+	
+	public void StockTextReader(List<Stock> stockList){
+		
+	}
+	
+	public void OrderTextReader(List<Order> orderList){
+	
+	}
+	
+	public void SoldTextReader(List<Sell> soldList){
+	
+	}
+
+}
