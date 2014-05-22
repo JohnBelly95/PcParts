3bd5ec65b9533d68676e59c036615b79d4b9a20d
dorarhcp <doradervi@live.com>
2014-05-22 22:00:12 +0300

    changes

--- TextReader.java
+++ TextReader.java
@@ -8,12 +8,47 @@ public class TextReader{
 	private List<Sell> soldList;
 	static BufferedReader input;
 	
+	String line;
+	BufferReader reader = null;
+	
 	public void StockTextReader(List<Stock> stockList){
 		
 	}
 	
 	public void OrderTextReader(List<Order> orderList){
-	
+		/*try{
+			elegxos an oi paraggelies perilamvanontai sta diathesima proionta
+		}
+		catch(){
+			eidopoihsh
+		}*/
+		
+		try{
+			line = reader.readLine();
+			while (line !=null){
+				if (!line.trim().equals(" ")) {
+					if (line.trim().equals("ORDER_LIST")) {
+						line = reader.readLine();
+						if (line != null){
+							if (line.trim().equals("{")) {
+								line = reader.readLine();
+								if (line != null){
+									if (line.trim().equals("ORDER")) {
+										line = reader.readLine();
+										if (line != null){
+											if (line.trim().equals("{")) {
+												line = reader.readLine();
+												//anagnorisi metavlitwn
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 	
 	public void SoldTextReader(List<Sell> soldList){
