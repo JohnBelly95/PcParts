ad1815ca64a02d1841aed72851ad01999c01c52c
JohnBelly95 <johnbelly95@gmail.com>
2014-05-24 23:32:40 +0300

    2332

--- TextReader2.java
+++ TextReader2.java
@@ -23,65 +23,67 @@ public class TextReader2{
 		}
 		try {
 			reader = new BufferedReader(new FileReader(f));
+			System.out.println("The file has opened.");
 		} catch (FileNotFoundException e) {
 			System.err.println("Error opening stock file!");
 		}
 		try{
 			line = reader.readLine();
-			if(line == null) continue;
-			System.out.println("The file has opened.");
-			if (!line.trim().equals(" ")) {
-				if (line.trim().startsWith("STOCK_LIST")) {
-					line = reader.readLine();
-					if (line == null) continue;
-					if (line.trim().equals("{")) {
-						while(line != null){
-							line = reader.readLine();
-							if(line == null) continue;
-							if (line.trim().startsWith("ITEM")) {
-								line = reader.readLine();
-								if (line == null) continue;
-								if (line.trim().equals("{")) {
-									while(line != "}"){
+			if(line == null){
+				if (!line.trim().equals(" ")) {
+					if (line.trim().startsWith("STOCK_LIST")) {
+						line = reader.readLine();
+						if (line == null){
+							if (line.trim().equals("{")) {
+								while(line != null){
+									line = reader.readLine();
+									if(line == null) continue;
+									if (line.trim().startsWith("ITEM")) {
 										line = reader.readLine();
-										String split[] = line.trim().split(":").trim();
-										split[0] = split[0].toUpperCase();
-										map.put(split[0],split[1]);
-									}
-									if(map.containsKey("TYPE")){
-										if(map.get("TYPE").equalsIgnoreCase("CPU")){
-											product = new CPU();
-											readCPU(1);
-										}else if(map.get("TYPE").equalsIgnoreCase("GPU")){
-											product = new GPU();
-											readGPU(1);
-										}else if(map.get("TYPE").equalsIgnoreCase("MOTHERBOARD")){
-											product = new Motherboard();
-											readMOBO(1);
-										}else if(map.get("TYPE").equalsIgnoreCase("RAM")){
-											product = new RAM();
-											readRAM(1);
-										}else if(map.get("TYPE").equalsIgnoreCase("HARDDRIVE")){
-											product = new HardDrive();
-											readHD(1);
-										}else if(map.get("TYPE").equalsIgnoreCase("KEYBOARD")){
-											product = new Keyboard();
-											readKeyboard(1);
-										}else if(map.get("TYPE").equalsIgnoreCase("MOUSE")){
-											product = new Mouse();
-											readMouse(1);
-										}else if(map.get("TYPE").equalsIgnoreCase("MONITOR")){
-											product = new Screen();
-											readMonitor(1);
-										}else if(map.get("TYPE").equalsIgnoreCase("PRINTER")){
-											product = new Printer();
-											readPrinter(1);
+										if (line == null) continue;
+										if (line.trim().equals("{")) {
+											while(line != "}"){
+												line = reader.readLine();
+												String split[] = line.trim().split(":");
+												split[0] = split[0].toUpperCase();
+												map.put(split[0],split[1]);
+											}
+											if(map.containsKey("TYPE")){
+												if(map.get("TYPE").equalsIgnoreCase("CPU")){
+													product = new CPU();
+													readCPU(1);
+												}else if(map.get("TYPE").equalsIgnoreCase("GPU")){
+													product = new GPU();
+													readGPU(1);
+												}else if(map.get("TYPE").equalsIgnoreCase("MOTHERBOARD")){
+													product = new Motherboard();
+													readMOBO(1);
+												}else if(map.get("TYPE").equalsIgnoreCase("RAM")){
+													product = new RAM();
+													readRAM(1);
+												}else if(map.get("TYPE").equalsIgnoreCase("HARDDRIVE")){
+													product = new HardDrive();
+													readHD(1);
+												}else if(map.get("TYPE").equalsIgnoreCase("KEYBOARD")){
+													product = new Keyboard();
+													readKeyboard(1);
+												}else if(map.get("TYPE").equalsIgnoreCase("MOUSE")){
+													product = new Mouse();
+													readMouse(1);
+												}else if(map.get("TYPE").equalsIgnoreCase("MONITOR")){
+													product = new Screen();
+													readMonitor(1);
+												}else if(map.get("TYPE").equalsIgnoreCase("PRINTER")){
+													product = new Printer();
+													readPrinter(1);
+												}
+											}else System.out.println("There is no item defined in this product");
+											line = reader.readLine();
+											map.clear();
+											if(line.trim().equals("}")) break;
+											
 										}
-									}else System.out.println("There is no item defined in this product");
-									line = readLine();
-									map.clear();
-									if(line.trim().equals("}")) break;
-									
+									}
 								}
 							}
 						}
@@ -250,20 +252,20 @@ public class TextReader2{
 		product.setmodelName(map.get("MODEL_NAME"));
 		product.setManufacturer(map.get("MANUFACTURER"));
 		product.setPrice(Integer.parseInt(map.get("PRICE")));
-		product.setYear(Integer.parseInt(map.get("YEAR")));
+		product.setmodelYear(Integer.parseInt(map.get("YEAR")));
 		((CPU)product).setSpeed(Double.parseDouble(map.get("SPEED")));
 		((CPU)product).setCores(Integer.parseInt(map.get("CORES")));
-		if(i=1){
+		if(i==1){
 			stk.setAvailableStock(Integer.parseInt(map.get("PIECES")));
-		}else if(i=2){
+		}else if(i==2){
 			ord.setName(map.get("NAME"));
-			ord.setPhone(map.get("PHONE"));
+			ord.setPhone(Long.parseLong(map.get("PHONE")));
 			ord.setFP(Double.parseDouble(map.get("FINAL_PRICE")));
 			ord.setExpectedDate(map.get("ARRIVAL_DATE"));
-		}else if(i=3){
+		}else if(i==3){
 			sl.setName(map.get("NAME"));
-			sl.setPhone(map.get("PHONE"));
-			sl.setFP(Double.parseDouble(map.get("FINAL_PRICE")));
+			sl.setPhone(Long.parseLong(map.get("PHONE")));
+			sl.setFp(Double.parseDouble(map.get("FINAL_PRICE")));
 		}
 	}
 }
